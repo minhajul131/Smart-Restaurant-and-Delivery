078c6952730ca95edfe6dbb1e649c61e9d74a5ca
Minhajul Islam <36797565+minhajul131@users.noreply.github.com>
2021-12-29 21:33:19 +0600

    1st commit
    try to comit

---
+++ home.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Smart Restaurant & Delivery</title>
+
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <!-- navber start-->
+    <section class = "navbar">
+        <div class = "container">
+            <div class = "logo">
+                <img src = "images/lx.png" alt = "page logo" class= "img-responsive">
+
+            </div>
+
+            <div class = "menu text-right">
+                <ul>
+                    <li>
+                        <a href = "#"> Home </a>
+                    </li>
+                    <li>
+                        <a href = "#"> Category </a>
+                    </li>
+                    <li>
+                        <a href = "#"> Order </a>
+                    </li>
+                    <li>
+                        <a href = "#"> Table </a>
+                    </li>
+                    <li>
+                        <a href = "#"> Contact Us </a>
+                    </li>
+                </ul>
+            </div>
+
+            <div class = "clearfix"></div>
+        </div>
+    </section>
+    <!-- navber ends-->
+
+    <!-- FoodSearch start-->
+    <section class = "food-search text-center">
+        <div class = "container">
+            <form action="">
+                <input type="search" name = "search" placeholder = "Search for food">
+                <input type="submit" name = "submit" value = "search" class= "btn btn-primary">
+            </form>
+        </div>
+    </section>
+    <!-- FoodSearch ends-->
+
+    <!-- category start-->
+    <section class = "category">
+        <div class = "container">
+            <h2 class = "text-center">Categories</h2>
+
+            <div class = "box-3 float-container" >
+                <img src="images/Pizza.jpg" alt="pizza" class = "img-responsive">
+                <h3 class = "float-text">Title</h3>
+            </div>
+            <div class = "box-3">
+                Box 2
+            </div>
+            <div class = "box-3">
+                Box 3
+            </div>
+
+            <div class = "clearfix"></div>
+
+        </div>
+
+    </section>
+    <!-- category ends-->
+
+    <!-- offer start-->
+    <!--<section class = "offer">
+        <div class = "container">
+            <h2 class = "text-center">Offers</h2>
+            <div class = "offer-box">
+                offer 1
+            </div>
+            <div class = "offer-box">
+                offer 2
+            </div>
+            <div class = "offer-box">
+                offer 3
+            </div>
+            <div class = "offer-box">
+                offer 4
+            </div>
+            <div class = "offer-box">
+                offer 5
+            </div>
+            <div class = "clearfix"></div>
+        </div>
+    </section>-->
+    <!-- offer ends-->
+
+    <!-- footer start-->
+    <section class = "footer">
+        <div class = "container">
+            <p>Developed by <a href="#">Minhajul Islam</a>. All rights reserved.</p>
+        </div>
+    </section>
+    <!-- footer ends-->
+</body>
+
+</html>
