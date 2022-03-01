0cd327ccb42276d5e97b3c5103f162b01282d996
Minhajul Islam <36797565+minhajul131@users.noreply.github.com>
2022-03-01 15:24:19 +0600

    design

--- home.php
+++ home.php
@@ -437,71 +437,60 @@
 
         <div class="testimonials-slider swiper" data-aos="fade-up" data-aos-delay="100">
           <div class="swiper-wrapper">
+          <?php 
+
+            //Query to Get all Categories from Database
+            $sql = "SELECT * FROM testimonials";
+
+            //Execute Query
+            $res = mysqli_query($conn, $sql);
+
+            //Count Rows
+            $count = mysqli_num_rows($res);
+
+            //Check whether we have data in database or not
+            if($count>0)
+            {
+                //We have data in database
+                //get the data and display
+                while($row=mysqli_fetch_assoc($res))
+                {
+                    $c_id = $row['c_id'];
+                    $c_name = $row['c_name'];
+                    $c_subject = $row['c_subject'];
+                    $c_message = $row['c_message'];
+                    
+
+                    ?>
 
             <div class="swiper-slide">
               <div class="testimonial-item">
                 <p>
                   <i class="bx bxs-quote-alt-left quote-icon-left"></i>
-                  Proin iaculis purus consequat sem cure digni ssim donec porttitora entum suscipit rhoncus. Accusantium quam, ultricies eget id, aliquam eget nibh et. Maecen aliquam, risus at semper.
+                  <?php echo $c_message; ?>
                   <i class="bx bxs-quote-alt-right quote-icon-right"></i>
                 </p>
-                <img src="assets/img/testimonials/testimonials-1.jpg" class="testimonial-img" alt="">
-                <h3>Saul Goodman</h3>
-                <h4>Ceo &amp; Founder</h4>
+                <img src="assets/img/stay-healthy.png" class="testimonial-img" alt="">
+                <h3><?php echo $c_name; ?></h3>
+                <h4><?php echo $c_subject; ?></h4>
               </div>
             </div><!-- End testimonial item -->
+            <?php
 
-            <div class="swiper-slide">
-              <div class="testimonial-item">
-                <p>
-                  <i class="bx bxs-quote-alt-left quote-icon-left"></i>
-                  Export tempor illum tamen malis malis eram quae irure esse labore quem cillum quid cillum eram malis quorum velit fore eram velit sunt aliqua noster fugiat irure amet legam anim culpa.
-                  <i class="bx bxs-quote-alt-right quote-icon-right"></i>
-                </p>
-                <img src="assets/img/testimonials/testimonials-2.jpg" class="testimonial-img" alt="">
-                <h3>Sara Wilsson</h3>
-                <h4>Designer</h4>
-              </div>
-            </div><!-- End testimonial item -->
+                        }
+                    }
+                    else
+                    {
+                      
+                    }
+                    
+                ?>
 
-            <div class="swiper-slide">
-              <div class="testimonial-item">
-                <p>
-                  <i class="bx bxs-quote-alt-left quote-icon-left"></i>
-                  Enim nisi quem export duis labore cillum quae magna enim sint quorum nulla quem veniam duis minim tempor labore quem eram duis noster aute amet eram fore quis sint minim.
-                  <i class="bx bxs-quote-alt-right quote-icon-right"></i>
-                </p>
-                <img src="assets/img/testimonials/testimonials-3.jpg" class="testimonial-img" alt="">
-                <h3>Jena Karlis</h3>
-                <h4>Store Owner</h4>
-              </div>
-            </div><!-- End testimonial item -->
+            
 
-            <div class="swiper-slide">
-              <div class="testimonial-item">
-                <p>
-                  <i class="bx bxs-quote-alt-left quote-icon-left"></i>
-                  Fugiat enim eram quae cillum dolore dolor amet nulla culpa multos export minim fugiat minim velit minim dolor enim duis veniam ipsum anim magna sunt elit fore quem dolore labore illum veniam.
-                  <i class="bx bxs-quote-alt-right quote-icon-right"></i>
-                </p>
-                <img src="assets/img/testimonials/testimonials-4.jpg" class="testimonial-img" alt="">
-                <h3>Matt Brandon</h3>
-                <h4>Freelancer</h4>
-              </div>
-            </div><!-- End testimonial item -->
+            
 
-            <div class="swiper-slide">
-              <div class="testimonial-item">
-                <p>
-                  <i class="bx bxs-quote-alt-left quote-icon-left"></i>
-                  Quis quorum aliqua sint quem legam fore sunt eram irure aliqua veniam tempor noster veniam enim culpa labore duis sunt culpa nulla illum cillum fugiat legam esse veniam culpa fore nisi cillum quid.
-                  <i class="bx bxs-quote-alt-right quote-icon-right"></i>
-                </p>
-                <img src="assets/img/testimonials/testimonials-5.jpg" class="testimonial-img" alt="">
-                <h3>John Larson</h3>
-                <h4>Entrepreneur</h4>
-              </div>
-            </div><!-- End testimonial item -->
+            
 
           </div>
           <div class="swiper-pagination"></div>
@@ -746,7 +735,7 @@
 
         <div class="col-lg-8 mt-5 mt-lg-0">
 
-        <form action="" method="POST" class="php-email-form">
+        <form action="" method="POST" >
           <div class="row">
             <div class="col-md-6 form-group">
               <input type="text" name="c_name" class="form-control" id="name" placeholder="Your Name" required>
@@ -761,8 +750,9 @@
           <div class="form-group mt-3">
             <textarea class="form-control" name="c_message" rows="8" placeholder="Message" required></textarea>
           </div>
+          <div class="my-3"></div>
                         
-          <div class="text-center"><button type="submit" name="submit">Send Message</button></div>
+          <div class="text-center"><button type="submit" name="submit" class="btn btn-primary  col-md-5">Send Message</button></div>
         </form>
 
         <?php 
